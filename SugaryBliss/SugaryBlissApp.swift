208c36e2347e269dd8fdc39c4100bf66a620dde3
cesarlopezt <cesarlopez2808@gmail.com>
2023-05-11 20:02:44 -0500

    Initial Commit

---
+++ SugaryBliss/SugaryBlissApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugaryBlissApp.swift
+//  SugaryBliss
+//
+//  Created by Cesar Lopez on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SugaryBlissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
